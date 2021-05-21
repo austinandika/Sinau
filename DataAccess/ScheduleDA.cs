73d86bb112a01c4a349ef0f3e99f8b99d9972e6b
austinandika <austinandika@gmail.com>
2021-05-21 20:48:08 +0700

    Add student schedule page backend

---
+++ DataAccess/ScheduleDA.cs
@@ -0,0 +1,66 @@
+﻿using Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ScheduleDA
+    {
+        private Database db;
+        public ScheduleDA()
+        {
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("SinauConnectionString");
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public List<ScheduleData> GetStudentScheduleDataByID(string userID)
+        {
+            string spName = "SP_GetStudentScheduleById";
+            List<ScheduleData> returnList;
+
+            try
+            {
+                DbCommand cmd = db.GetStoredProcCommand(spName);
+                db.AddInParameter(cmd, "UserID", System.Data.DbType.String, userID);
+
+                using (IDataReader reader = db.ExecuteReader(cmd))
+                {
+                    List<ScheduleData> studentScheduleList = new List<ScheduleData>();
+
+                    while (reader.Read())
+                    {
+                        ScheduleData data = new ScheduleData();
+                        data._DayID = Convert.ToString(reader["DayID"]);
+                        data._SubjectName = Convert.ToString(reader["SubjectName"]);
+
+                        data._TimeStart = TimeSpan.Parse(reader["TimeStart"].ToString()).ToString(@"hh\:mm");
+                        data._TimeEnd = TimeSpan.Parse(reader["TimeEnd"].ToString()).ToString(@"hh\:mm");
+                        data._LinkVidcon = Convert.ToString(reader["LinkVidcon"]);
+
+                        studentScheduleList.Add(data);
+                    }
+
+                    returnList = studentScheduleList;
+                }
+                return returnList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
--- Sinau/View/Student/Schedule.aspx.cs
+++ Sinau/View/Student/Schedule.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessFacade;
+using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,89 @@ namespace Sinau.View.Student
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string sessionUserID = Session["UserID"].ToString();
+
+            try
+            {
+                List<ScheduleData> listSchedule = new ScheduleSystem().GetStudentScheduleDataByID(sessionUserID);
+
+                if(listSchedule != null)
+                {
+                    List<ScheduleData> listScheduleMonday = listSchedule.Where(p => p._DayID == "Day1").ToList();
+                    if(listScheduleMonday.Count != 0)
+                    {
+                        rptScheduleMonday.DataSource = listScheduleMonday;
+                        rptScheduleMonday.DataBind();
+                    }
+                    else
+                    {
+                        noScheduleDay1.Attributes["class"] += " active";
+                    }
+
+                    List<ScheduleData> listScheduleTuesday = listSchedule.Where(p => p._DayID == "Day2").ToList();
+                    if (listScheduleTuesday.Count != 0)
+                    {
+                        rptScheduleTuesday.DataSource = listScheduleTuesday;
+                        rptScheduleTuesday.DataBind();
+                    }
+                    else
+                    {
+                        noScheduleDay2.Attributes["class"] += " active";
+                    }
+
+                    List<ScheduleData> listScheduleWednesday = listSchedule.Where(p => p._DayID == "Day3").ToList();
+                    if (listScheduleWednesday.Count != 0)
+                    {
+                        rptScheduleWednesday.DataSource = listScheduleWednesday;
+                        rptScheduleWednesday.DataBind();
+                    }
+                    else
+                    {
+                        noScheduleDay3.Attributes["class"] += " active";
+                    }
+
+                    List<ScheduleData> listScheduleThursday = listSchedule.Where(p => p._DayID == "Day4").ToList();
+                    if (listScheduleThursday.Count != 0)
+                    {
+                        rptScheduleThursday.DataSource = listScheduleThursday;
+                        rptScheduleThursday.DataBind();
+                    }
+                    else
+                    {
+                        noScheduleDay4.Attributes["class"] += " active";
+                    }
+
+                    List<ScheduleData> listScheduleFriday = listSchedule.Where(p => p._DayID == "Day5").ToList();
+                    if (listScheduleFriday.Count != 0)
+                    {
+                        rptScheduleFriday.DataSource = listScheduleFriday;
+                        rptScheduleFriday.DataBind();
+                    }
+                    else
+                    {
+                        noScheduleDay5.Attributes["class"] += " active";
+                    }
+
+                    List<ScheduleData> listScheduleSaturday = listSchedule.Where(p => p._DayID == "Day6").ToList();
+                    if (listScheduleSaturday.Count != 0)
+                    {
+                        rptScheduleSaturday.DataSource = listScheduleSaturday;
+                        rptScheduleSaturday.DataBind();
+                    }
+                    else
+                    {
+                        noScheduleDay6.Attributes["class"] += " active";
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+
 
         }
     }
